14fe800bd38c2ce3c6f98d29ef8172e1d39e388e
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-08-17 23:19:20 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryClick);
+
+function createGalleryItems(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+</div>
+    `;
+    })
+    .join("");
+}
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+}
+
+//
+// function onOpenImage() {
+//   document.body.classList.add("gallery__image");
+// }
